38f947e918561a89f455607bac9a9b4796390283
hx <1317205072@qq.com>
2020-11-02 10:56:42 +0800

    更新1.0.3

--- unpackage/dist/dev/mp-alipay/.tea/entryFiles-development/index$.web.js
+++
@@ -1,5 +0,0 @@
-require('@alipay/appx-compiler/lib/sjsEnvInit');
-require('./config$');
-
-require('../../components/beauty-calendar/beauty-calendar?hash=ec7c5687c7f2ffa836caf3a93e877914426baff6');
-require('../../pages/index/index?hash=575295ce50b33b83b61e5dd1e6931abd30ffc51d');
